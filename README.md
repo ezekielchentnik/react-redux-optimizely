174399f5681599f19f2998a3acd177b240110037
ezekielchentnik <zeek@ezekielchentnik.com>
2016-07-16 23:00:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# react-redux-optimizely
+
+[![Build Status](https://travis-ci.org/ezekielchentnik/react-redux-optimizely.svg)](https://travis-ci.org/ezekielchentnik/react-redux-optimizely)
+[![npm version](https://img.shields.io/npm/v/react-redux-optimizely.svg?style=flat-square)](https://www.npmjs.com/package/react-redux-optimizely)
+
+React Component(s) and helpers for running Optimizely experiments in a React/Redux Application.
+
+## Installation
+
+```js
+npm install --save react-redux-optimizely
+```
+
+### Connecting an experiment
+```js
+// todo
+```
+
+### Configuring Optimizely
+```js
+// todo
+```
+
+### Experiment Conditions
+```js
+// todo
+```
+
+### Activate Experiment on render
+```js
+// todo
+```
+
+## Configure Redux Middleware
+```js
+// todo
+```
+
+### Goal Tracking
+```js
+// todo
+```
---
+++ src/index.js
@@ -0,0 +1,84 @@
+import reporter from 'redux-reporter';
+import React, { Component, PropTypes } from 'react';
+
+function getActiveExperiments() {
+    return (window['optimizely'] && window['optimizely'].activeExperiments) ? window['optimizely'].activeExperiments : []; // eslint-disable-line
+}
+
+function getVariationMap() {
+    return window['optimizely'].variationMap || {}; // eslint-disable-line
+}
+
+function isEnabled(EXPERIMENT_ID) {
+    return true; // todo, actually check, although this is an extraneious check
+}
+
+function getExperimentId(EXPERIMENT_NAME) {
+    // todo
+}
+
+function isActive(EXPERIMENT_ID) {
+    const activeExperiments = getActiveExperiments();
+    const found = activeExperiments.filter((activeExperimentId) => {
+        return activeExperimentId === EXPERIMENT_ID;
+    });
+    return !!found.length;
+}
+
+export function getVariant(EXPERIMENT_ID) {
+    const variationMap = getVariationMap();
+    if(isEnabled(EXPERIMENT_ID) && isActive(EXPERIMENT_ID)){
+        return  variationMap[EXPERIMENT_ID];
+    }
+    return null;
+}
+
+export function activate(EXPERIMENT_ID) {
+
+    if(!isEnabled(EXPERIMENT_ID)){
+        return false;
+    }
+
+    if(!isActive(EXPERIMENT_ID)){ // only activate, enabled experiment
+        window['optimizely'] = window['optimizely'] || []; // eslint-disable-line
+        window['optimizely'].push(["activate", EXPERIMENT_ID]); // eslint-disable-line
+    }
+
+    return isActive(EXPERIMENT_ID);
+}
+
+const connectExperiment = (experimentId, suppressActivateOnRender = false) => (Wrapped) => {
+
+    class Wrapper extends Component {
+
+        render() {
+            let isActive = false;
+            if(!suppressActivateOnRender){
+                isActive = activate(experimentId);
+            }
+
+            const variant = getVariant(experimentId);
+
+            return (<Wrapped isActive={isActive}
+                    variant={variant} {...this.props} />);
+
+        }
+    }
+
+    Wrapper.propTypes = {
+        experimentId: PropTypes.string,
+        suppressActivateOnRender: PropTypes.bool
+    };
+
+    return Wrapper;
+};
+
+const goalMiddleware = reporter(({ type, payload }) => {
+
+    window.optimizely = window.optimizely || [];
+    window.optimizely.push(['trackEvent', type, payload]);
+
+}, ({ meta = {} }) => meta.experiments);
+
+export default connectExperiment;
+export { goalMiddleware };
